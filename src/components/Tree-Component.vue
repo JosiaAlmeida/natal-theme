3d849cc7f5e67f01f0d9786fed47a425614c7050
Josia Almeida <josiaalmeida0001@gmail.com>
2022-12-11 19:51:47 +0100

    Ajustando video para iphone

--- src/components/Tree-Component.vue
+++ src/components/Tree-Component.vue
@@ -1,7 +1,9 @@
 <template>
-  <video class="video-main" autoplay loop muted>
-    <source src="../assets/christmas5.mp4" type="video/mp4" />
-  </video>
+  <div class="video-container">
+    <video class="video-main" playsinline="" preload="auto" autoplay loop muted>
+      <source src="..//assets/christmas5.mp4" type="video/mp4" />
+    </video>
+  </div>
 </template>
 
 <script>
@@ -9,12 +11,25 @@ export default {};
 </script>
 
 <style lang="css" scoped>
-.video-main {
+.video-container {
   position: fixed;
   right: 0;
   bottom: 0;
+  height: 100vh;
+  width: 100vw;
+}
+.video-main {
   min-width: 100%;
   min-height: 100%;
-  filter: brightness(0.6);
+  filter: brightness(0.8);
+}
+@media screen and (max-width: 920px) {
+  .video-container {
+    left: 0 !important;
+    right: none;
+    margin-left: calc(100vw - 200%);
+  }
+  .video-main {
+  }
 }
 </style>
